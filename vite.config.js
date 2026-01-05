5119e1692592a190590b591d73ed8958c4459d96
hosam740 <hosam142505@gmail.com>
2026-01-05 18:40:07 +0300

    Fix Vite inputs for auth and app layouts

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,19 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                // Global
                 'resources/css/base.css',
+
+                // App (dashboard)
                 'resources/css/app.css',
                 'resources/js/app.js',
+
+                // Auth
+                'resources/css/auth.css',
+                'resources/js/auth.js',
+
+                // Bootstrap
+                'resources/js/bootstrap.js',
             ],
             refresh: true,
         }),
